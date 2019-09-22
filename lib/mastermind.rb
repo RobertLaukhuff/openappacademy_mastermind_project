a515a4bea74253ab65484f3b1651d6bd6e9229e0
Robert Laukhuff <robert@laukcoder.com>
2019-09-22 00:06:43 -0400

    Build functionality of Mastermind class

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,5 +1,20 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end
 
+  def print_matches(code)
+    print "exact matches: #{@secret_code.num_exact_matches(code)}"
+    print "near matches: #{@secret_code.num_near_matches(code)}"
+  end
+
+  def ask_user_for_guess
+    print "Enter a code: "
+    pegs = gets.chomp.upcase
+    guess = Code.from_string(pegs)
+    print_matches(guess)
+    guess == @secret_code
+  end
 end
